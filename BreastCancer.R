62558b378102cb897a074779e4c674e3786542ee
Garmire group <lgarmire@gmail.com>
2020-12-02 12:27:09 -0500

    Add files via upload

---
+++ BreastCancer.R
@@ -0,0 +1,108 @@
+#Download datasets GSE113197 and GSE123926 from GEO before running this script.
+
+library('Asgard')
+
+setwd("Your_local_path/")
+
+#Load normal sample Ind5 from GSE113197 dataset
+celltype<-read.table(file="Normal_celltype.txt",header = T,check.names=FALSE)
+data<-read.table(file="GSM3099847_Ind5_Expression_Matrix.txt",header = T,check.names=FALSE)
+row.names(data)<-data[,1]
+data<-data[,-1]
+celltype2<-subset(celltype,sample=="Ind5" & celltype %in% c("Luminal_L2_epithelial_cells","Luminal_L1.1_epithelial_cells", "Luminal_L1.2_epithelial_cells", "Basal_epithelial_cells"))
+common <- intersect(colnames(data), rownames(celltype2))
+data<-data[,common]
+Epithelial2 <- CreateSeuratObject(counts = data, project = "Epithelial", min.cells = 3, min.features = 200,meta.data=data.frame(celltype2,cell=colnames(data),type="Normal"))
+
+#Load normal sample Ind6 from GSE113197 dataset
+data<-read.table(file="GSM3099848_Ind6_Expression_Matrix.txt",header = T,check.names=FALSE)
+row.names(data)<-data[,1]
+data<-data[,-1]
+celltype3<-subset(celltype,sample=="Ind6" & celltype %in% c("Luminal_L2_epithelial_cells","Luminal_L1.1_epithelial_cells", "Luminal_L1.2_epithelial_cells", "Basal_epithelial_cells"))
+common <- intersect(colnames(data), rownames(celltype3))
+data<-data[,common]
+Epithelial3 <- CreateSeuratObject(counts = data, project = "Epithelial", min.cells = 3, min.features = 200,meta.data=data.frame(celltype3,cell=colnames(data),type="Normal"))
+
+#Load normal sample Ind7 from GSE113197 dataset
+data<-read.table(file="GSM3099849_Ind7_Expression_Matrix.txt",header = T,check.names=FALSE)
+row.names(data)<-data[,1]
+data<-data[,-1]
+celltype4<-subset(celltype,sample=="Ind7" & celltype %in% c("Luminal_L2_epithelial_cells","Luminal_L1.1_epithelial_cells", "Luminal_L1.2_epithelial_cells", "Basal_epithelial_cells"))
+common <- intersect(colnames(data), rownames(celltype4))
+data<-data[,common]
+Epithelial4 <- CreateSeuratObject(counts = data, project = "Epithelial", min.cells = 3, min.features = 200,meta.data=data.frame(celltype4,cell=colnames(data),type="Normal"))
+
+#Load cancer sample PDX110 from GSE123926 dataset
+TNBC_PDX.data<- Read10X(data.dir = "GSM3516947_PDX110")
+TNBC.PDX2 <- CreateSeuratObject(counts = TNBC_PDX.data, project = "TNBC", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(TNBC_PDX.data),cell=colnames(TNBC_PDX.data),sample="PDX-110",type="TNBC.PDX"))
+
+#Load cancer sample PDX322 from GSE123926 dataset
+TNBC_PDX.data<- Read10X(data.dir = "GSM3516948_PDX322")
+TNBC.PDX3 <- CreateSeuratObject(counts = TNBC_PDX.data, project = "TNBC", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(TNBC_PDX.data),cell=colnames(TNBC_PDX.data),sample="PDX-332",type="TNBC.PDX"))
+
+#single-cell alignment
+SC.list<-list(TNBC.PDX2=TNBC.PDX2,TNBC.PDX3=TNBC.PDX3,Epithelial2=Epithelial2,Epithelial3=Epithelial3,Epithelial4=Epithelial4)
+SC.data<-SCalignment(SC.list)
+
+#Change sample names
+sample<-SC.data@meta.data$sample
+sample[which(sample=="Ind5")]<-"Normal1"
+sample[which(sample=="Ind6")]<-"Normal2"
+sample[which(sample=="Ind7")]<-"Normal3"
+SC.data@meta.data$sample<-sample
+
+#View alignment results
+DimPlot(SC.data, reduction = "umap", split.by = "sample", label = TRUE)
+
+#Save data
+saveRDS(SC.data,file="TNBC_SCdata.rds")
+
+#Get differential genes
+Case=c("PDX-110","PDX-332")
+Control=c("Normal1","Normal2","Normal3")
+Gene.list<-GetGene(SC.integrated=SC.data,Case=Case,Control=Control,min.cells=3)
+#Save data
+saveRDS(Gene.list,file="TNBC_genelist.rds")
+
+
+#Drug repurposing
+Gene.list<-readRDS("TNBC_genelist.rds")
+my_gene_info<-read.table(file="Your_loacal_path_for_drug_reference/breast_gene_info.txt",sep="\t",header = T,quote = "")
+my_drug_info<-read.table(file="Your_loacal_path_for_drug_reference/breast_drug_info.txt",sep="\t",header = T,quote = "")
+drug.ref.profiles = GetDrugRef(drug.response.path = 'Your_loacal_path_for_drug_reference/breast_rankMatrix.txt',
+                                 probe.to.genes = my_gene_info, drug.info = my_drug_info)
+Drug.ident.res = GetDrug(gene.data = Gene.list, drug.ref.profiles = drug.ref.profiles, repurposing.unit = "drug", connectivity = "negative", drug.type = "all")
+saveRDS(Drug.ident.res,file="TNBC_drugs.rds")
+
+#Drug combination
+SC.data<-readRDS("TNBC_SCdata.rds")
+Gene.list<-readRDS("TNBC_genelist.rds")
+Drug.ident.res<-readRDS("TNBC_drugs.rds")
+GSE92742.gctx.path="C:/Projects/Drug_reposition/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx"
+GSE70138.gctx.path="C:/Projects/Drug_reposition/GSE70138_Broad_LINCS_Level5_COMPZ_n118050x12328.gctx"
+Tissue="breast"
+Drug.combinations<-DrugCombination(SC.integrated=SC.data,
+                      Gene.data=Gene.list,
+                      Drug.data=Drug.ident.res,
+                      Drug.FDR=0.1,
+                      FDA.drug.only=TRUE,
+                      Combined.drugs=2,
+                      Case=Case,
+                      Tissue="breast",
+                      GSE92742.gctx=GSE92742.gctx.path,
+                      GSE70138.gctx=GSE70138.gctx.path)
+saveRDS(Drug.combinations,file="TNBC_drugcombinations.rds")
+
+#Select mono-drugs
+Final.drugs<-TopDrug(SC.integrated=SC.data,
+                   Drug.data=Drug.ident.res,
+                   Drug.FDR=0.1,
+                   FDA.drug.only=TRUE,
+                   Case=Case
+)
+
+#Select drug combinations
+Final.combinations<-TopCombination(Drug.combination=Drug.combinations,
+                   Combination.FDR=0.1,
+                   Min.combination.score=1
+)
---
+++ COVID19.R
@@ -0,0 +1,129 @@
+#Download dataset GSE145926 from GEO before running this script.
+
+library('Asgard')
+
+setwd("Your_local_path/")
+
+#Load data
+data<-Read10X_h5(file="GSM4339769_C141_filtered_feature_bc_matrix.h5")
+C141 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C141",type="Mild.COVID19",outcome="Cured"))
+C141[["percent.mt"]] <- PercentageFeatureSet(C141, pattern = "^MT-")
+C141 <- subset(C141, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4339770_C142_filtered_feature_bc_matrix.h5")
+C142 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C142",type="Mild.COVID19",outcome="Cured"))
+C142[["percent.mt"]] <- PercentageFeatureSet(C142, pattern = "^MT-")
+C142 <- subset(C142, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4339771_C143_filtered_feature_bc_matrix.h5")
+C143 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C143",type="Severe.COVID19",outcome="Death"))
+C143[["percent.mt"]] <- PercentageFeatureSet(C143, pattern = "^MT-")
+C143 <- subset(C143, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4339772_C144_filtered_feature_bc_matrix.h5")
+C144 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C144",type="Mild.COVID19",outcome="Cured"))
+C144[["percent.mt"]] <- PercentageFeatureSet(C144, pattern = "^MT-")
+C144 <- subset(C144, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4339773_C145_filtered_feature_bc_matrix.h5")
+C145 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C145",type="Severe.COVID19",outcome="Cured"))
+C145[["percent.mt"]] <- PercentageFeatureSet(C145, pattern = "^MT-")
+C145 <- subset(C145, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4339774_C146_filtered_feature_bc_matrix.h5")
+C146 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C146",type="Severe.COVID19",outcome="Death"))
+C146[["percent.mt"]] <- PercentageFeatureSet(C146, pattern = "^MT-")
+C146 <- subset(C146, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4475048_C51_filtered_feature_bc_matrix.h5")
+C51 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="Healthy.C51",type="Healthy",outcome="Healthy"))
+C51[["percent.mt"]] <- PercentageFeatureSet(C51, pattern = "^MT-")
+C51 <- subset(C51, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4475049_C52_filtered_feature_bc_matrix.h5")
+C52 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="Healthy.C52",type="Healthy",outcome="Healthy"))
+C52[["percent.mt"]] <- PercentageFeatureSet(C52, pattern = "^MT-")
+C52 <- subset(C52, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4475050_C100_filtered_feature_bc_matrix.h5")
+C100 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="Healthy.C100",type="Healthy",outcome="Healthy"))
+C100[["percent.mt"]] <- PercentageFeatureSet(C100, pattern = "^MT-")
+C100 <- subset(C100, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4475051_C148_filtered_feature_bc_matrix.h5")
+C148 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C148",type="Severe.COVID19",outcome="Cured"))
+C148[["percent.mt"]] <- PercentageFeatureSet(C148, pattern = "^MT-")
+C148 <- subset(C148, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4475052_C149_filtered_feature_bc_matrix.h5")
+C149 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C149",type="Severe.COVID19",outcome="Cured"))
+C149[["percent.mt"]] <- PercentageFeatureSet(C149, pattern = "^MT-")
+C149 <- subset(C149, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+data<-Read10X_h5(file="GSM4475053_C152_filtered_feature_bc_matrix.h5")
+C152 <- CreateSeuratObject(counts = data, project = "COVID19", min.cells = 3, min.features = 200,meta.data=data.frame(row.names=colnames(data),cell=colnames(data),sample="COVID19.C152",type="Severe.COVID19",outcome="Cured"))
+C152[["percent.mt"]] <- PercentageFeatureSet(C152, pattern = "^MT-")
+C152 <- subset(C152, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
+
+#Merge data
+Mild.COVID19<-merge(x=C141,y=c(C142,C144))
+Severe.COVID19<-merge(x=C143,y=c(C145,C146,C148,C149,C152))
+Healthy<-merge(x=C51,y=c(C52,C100))
+
+#single-cell alignment and annotation
+SC.list<-list(Mild.COVID19=Mild.COVID19,Severe.COVID19=Severe.COVID19,Healthy=Healthy)
+SC.data<-SCalignment(SC.list,by.CellType=TRUE)
+
+#Save data
+saveRDS(SC.data,file="COVID19_SCdata.rds")
+
+#Creat the vector of sample names
+Mild.COVID19<-c("COVID19.C141","COVID19.C142","COVID19.C144")
+Severe.COVID19<-c("COVID19.C143","COVID19.C145","COVID19.C146","COVID19.C148","COVID19.C149","COVID19.C152")
+Healthy<-c("Healthy.C51","Healthy.C52","Healthy.C100")
+
+#Get differential genes
+Gene.list<-GetGene(SC.integrated=SC.data,Case=Severe.COVID19,Control=Mild.COVID19,min.cells=3)
+
+#Save data
+saveRDS(Gene.list,file="COVID19_genelist.rds")
+
+#Drug repurposing using FDA-approved drugs
+Gene.list<-readRDS("COVID19_genelist.rds")
+my_gene_info<-read.table(file="Your_loacal_path_for_drug_reference/lung_gene_info.txt",sep="\t",header = T,quote = "")
+my_drug_info<-read.table(file="Your_loacal_path_for_drug_reference/lung_drug_info.txt",sep="\t",header = T,quote = "")
+cmap.ref.profiles = GetDrugRef(drug.response.path = 'Your_loacal_path_for_drug_reference/lung_rankMatrix.txt',
+                                 probe.to.genes = my_gene_info, drug.info = my_drug_info)
+Drug.ident.res = GetDrug(gene.data = Gene.list, drug.ref.profiles = cmap.ref.profiles, repurposing.unit = "drug", connectivity = "negative", drug.type="FDA")
+saveRDS(Drug.ident.res,file="COVID19_drugs.rds")
+
+#Select FDA-approved mono-drugs
+Final.drugs<-TopDrug(SC.integrated=SC.data,
+                     Drug.data=Drug.ident.res,
+                     Drug.FDR=0.1,
+                     FDA.drug.only=TRUE,
+                     Case=Case
+)
+
+#Save data
+saveRDS(Final.drugs,file="COVID19_FDA_drugs.rds")
+
+#Drug repurposing using compounds
+Gene.list<-readRDS("COVID19_genelist.rds")
+my_gene_info<-read.table(file="Your_loacal_path_for_drug_reference/lung_gene_info.txt",sep="\t",header = T,quote = "")
+my_drug_info<-read.table(file="Your_loacal_path_for_drug_reference/lung_drug_info.txt",sep="\t",header = T,quote = "")
+cmap.ref.profiles = GetDrugRef(drug.response.path = 'Your_loacal_path_for_drug_reference/lung_rankMatrix.txt',
+                               probe.to.genes = my_gene_info, drug.info = my_drug_info)
+Drug.ident.res = GetDrug(gene.data = Gene.list, drug.ref.profiles = cmap.ref.profiles, repurposing.unit = "drug", connectivity = "negative", drug.type="compounds")
+saveRDS(Drug.ident.res,file="COVID19_drugs.rds")
+
+#Select mono-compounds
+Final.drugs<-TopDrug(SC.integrated=SC.data,
+                     Drug.data=Drug.ident.res,
+                     Drug.FDR=0.1,
+                     FDA.drug.only=FALSE,
+                     Case=Case
+)
+
+#Save data
+saveRDS(Final.drugs,file="COVID19_compounds.rds")
